a1120bbc54fa9a83d052ba053d3e793cda0a879a
masriadi_sisfo <113230323+masriadiNotDevv@users.noreply.github.com>
2026-09-02 23:16:24 +0800

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,6 +1,9 @@
 /*
+komentar di java tidak akan tereksekusi pada compiler,
+contoh komentar multiline
 day 1 java 
 */
+// contoh komentar single-line
 public class Day1 {  // nama class adalah Day1 dan di atur secara public supaya class lain bisa mengakses class day1 dan dalaam class tidak boleh membuat class baru harus di luar scope class ini
     public static void main(String[] args) { // isi di dalam method main adalah titik awal program di jalankan
         System.out.println("hello world");  // kode yg akan di eksekusi 
