2e01e29d9fc5a1a5210e25eb815423d2ed57a69a
masriadinotdevv <serangbee@gmail.com>
2026-09-02 20:35:58 +0800

    day 1

--- Day1.java
+++ Day1.java
@@ -1,8 +1,14 @@
 /*
-
+day 1 java 
 */
-public class Day1 { 
-    public static void main(String[] args) {
-        System.out.println("hello world"); 
+public class Day1 {  // nama class adalah Day1 dan di atur secara public supaya class lain bisa mengakses class day1 dan dalaam class tidak boleh membuat class baru harus di luar scope class ini
+    public static void main(String[] args) { // isi di dalam method main adalah titik awal program di jalankan
+        System.out.println("hello world");  // kode yg akan di eksekusi 
     }
 }
+
+/* *
+ public class cntoh {  //
+... dan di class cnth selagi di atur secara public maka class dari luar bisa mengakses class cnth 
+} 
+*/
