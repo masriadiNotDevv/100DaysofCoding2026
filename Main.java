6f0dffaafb9f6d69a62293d4094d7127f3e0226c
masriadinotdevv <serangbee@gmail.com>
2026-09-02 20:21:31 +0800

    initial commit

--- Main.java
+++ Main.java
@@ -1,23 +1,8 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-     while (true) {
-         System.out.print("masukan umur : ");
-         int umur = input.nextInt();
-       
-if (umur <= 0 || umur >= 80) {
-    System.out.println("umur tidak valid , max umur 18-80");
-    continue;
-} else if (umur < 18) {
-    System.out.println("lu masih bocil kontoll");
-    continue;
-} else if (umur >= 18) {
-    System.out.println("ok anda cukup umur");
-    break;
+    public class Main {
+        public static void main(String[] args) {
+int n = 1;
+for (int i = 5; i >= n; i--) {
+    System.out.println(i);
 }
-
-
-     }
-    }
+}
 }
