017e1f74ccc2dde9dadcddbd9cd0332e92319bec
masriadi_sisfo <113230323+masriadiNotDevv@users.noreply.github.com>
2026-09-08 17:55:17 +0800

    Create Day7.java for string data type introduction
    Added a Java class to demonstrate string data types and concatenation.

---
+++ Day7.java
@@ -0,0 +1,15 @@
+public class Day7 {
+    public static void main(String[] args) {
+        /*
+        day7 pengenalan type data untuk string
+        singkatnya string adalah typedata untuk yg isinya bernilai sangat panjang seperti menampung suatu kalimat atau teks 
+        */       
+
+        // penggunaannya 
+        String sapa = "halo";
+        String nama = "adi";
+
+        System.out.println(sapa + "" + nama); // output halo adi 
+        // simbol + di atas bukan menjumlahkan nilai dari sapa dan nama tapi menggabungkanya karena ke dua variabel ersebut bersifat string
+    }
+}
