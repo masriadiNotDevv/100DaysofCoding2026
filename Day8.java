891b7964e97625b5febbf8d239734788c1581445
masriadi_sisfo <113230323+masriadiNotDevv@users.noreply.github.com>
2026-09-09 19:30:47 +0800

    y

---
+++ Day8.java
@@ -0,0 +1,14 @@
+public class Day8 {
+
+public static void main(String[] args) {
+/*
+Deklarasi dan inisialisasi
+deklarasi = membuat variabel yg nilainy belum ada,
+  */
+
+  String nama; // ini namanya melakukann deklarasi tapi belum mempunyai milai
+
+  nama = "0xadi"; // ini inisialisasi, untuk memberikan nilai pertama ke dalam variabel nama
+
+}
+}
