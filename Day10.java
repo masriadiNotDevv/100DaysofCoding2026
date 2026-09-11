f03c64b85f0a79332986c5c418efdea935df650d
masriadi_sisfo <113230323+masriadiNotDevv@users.noreply.github.com>
2026-09-11 23:24:33 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Byte:");
+        System.out.println("Min = " + Byte.MIN_VALUE);
+        System.out.println("Max = " + Byte.MAX_VALUE);
+
+        System.out.println("\nShort:");
+        System.out.println("Min = " + Short.MIN_VALUE);
+        System.out.println("Max = " + Short.MAX_VALUE);
+
+        System.out.println("\nInteger:");
+        System.out.println("Min = " + Integer.MIN_VALUE);
+        System.out.println("Max = " + Integer.MAX_VALUE);
+
+        System.out.println("\nLong:");
+        System.out.println("Min = " + Long.MIN_VALUE);
+        System.out.println("Max = " + Long.MAX_VALUE);
+    }
+}
