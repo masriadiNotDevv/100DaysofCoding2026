917912271430045c2056209a152890f707d60a9a
masriadi_sisfo <113230323+masriadiNotDevv@users.noreply.github.com>
2026-09-15 20:51:52 +0700

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,14 @@
+public class Day14 {
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+
+        int hasilTambah = angka1 + angka2;
+        int hasilKurang = angka1 + angka2;
+
+        System.out.println(hasilTambah);
+        System.out.println(hasilKurang);
+        
+    }
+}
+
