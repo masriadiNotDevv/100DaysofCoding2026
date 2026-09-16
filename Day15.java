55b99f8003605dda44802025598af34dcd69927b
masriadi_sisfo <113230323+masriadiNotDevv@users.noreply.github.com>
2026-09-16 19:52:03 +0700

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,13 @@
+public class Day15 {
+    public static void main(String[] args) {
+        int angka1 = 20;
+        int angka2 = 10;
+
+        int hasilKali = angka1 * angka2;
+        int hasilBagi = angka1 / angka2;
+
+        System.out.println(hasilKali);
+        System.out.println(hasilBagi);
+    }
+}
+
