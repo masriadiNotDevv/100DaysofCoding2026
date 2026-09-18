c7b43ff748f6f33a63b5135aef359b4dd056ec07
masriadi_sisfo <113230323+masriadiNotDevv@users.noreply.github.com>
2026-09-18 10:35:18 +0700

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,11 @@
+public class Day17 {
+    public static void main(String[] args) {
+       int angka = 10;
+       System.out.println(angka += 10);
+       System.out.println(angka -= 5);
+       System.out.println(angka /= 3);
+       System.out.println(angka *= 10);
+       System.out.println(angka %= 30);
+    }
+}
+
