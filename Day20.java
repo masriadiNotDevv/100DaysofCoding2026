82476d62726dff0fe1a525dd03a2cb08349a3774
masriadi_sisfo <113230323+masriadiNotDevv@users.noreply.github.com>
2026-09-21 19:45:31 +0700

    Add Day20.java with integer to string conversion

---
+++ Day20.java
@@ -0,0 +1,9 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int a = 10;
+        String b = String.valueOf(a); // cara 1 
+        String c = "" + a; // cara 2
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
